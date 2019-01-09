ca9eb834522d81141594a8dcacf8de567235e368
bniestoj <barbara.niestoj@gmail.com>
2019-01-09 19:18:03 +0100

    getting elements from form and adding

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
 var allPersons = [];
 
 var Person = {
-    firstName: "John",
-    lastName : "Doe",
-  };
+  firstName: "John",
+  lastName: "Doe",
+};
 
 window.onload = () => {
 
 }
 
 function generateList(tab) {
-    tab.sort(function (a, b) {
-        if (a.firstName < b.firstName) {
-            return -1;
-        }
-        if (a.firstName > b.firstName){
-            return 1;
-        }
-        return 0;
-      });
-      for (var i =0; i <tab.length; i++){
-        $('ul').append('li').text(tab[i].firstName + ' ' + tab[i].lastName);
+  tab.sort(function (a, b) {
+    if (a.firstName < b.firstName) {
+      return -1;
+    }
+    if (a.firstName > b.firstName) {
+      return 1;
+    }
+    return 0;
+  });
+  for (var i = 0; i < tab.length; i++) {
+    $('ul').append('li').text(tab[i].firstName + ' ' + tab[i].lastName);
 
-      }
+  }
 
 
 
 
 }
 
-function addPerson(){
-  alert('aaa');
+function addPerson() {
+  var $input = $('#name');
+  var name = $input.val();
+  var $input = $('#lastname');
+  var lastname = $input.val();
+  var Person = {
+    firstName: name,
+    lastName: lastname
+  };
+  allPersons.push(Person);
+  generateList(allPersons);
 }
+
