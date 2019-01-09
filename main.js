a16091d3bef7b45e42c9c9c21f52bb359b1e08a7
Jacek <ja.staszak@gmail.com>
2019-01-09 19:57:26 +0100

    chyba ostateczne poprawki XD

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
+var allPersons = JSON.parse(localStorage.getItem("allPersons"));
+    if (allPersons == null) {
+        var allPersons = [];
+        localStorage.setItem("allPersons", JSON.stringify(allPersons));
+    }
+
 window.onload = () => {
-    var allPersons =  JSON.parse(localStorage.getItem("allPersons"));
+
     generateList(allPersons);
+    
 }
 
 function generateList(tab) {
     var $ul = $('<ul></ul>');
     tab.sort(function (a, b) {
-        if (a.lastName < b.lastName) {
+        if (a.lastName.toLowerCase() < b.lastName.toLowerCase()) {
             return -1;
         }
-        if (a.lastName > b.lastName){
+        if (a.lastName.toLowerCase() > b.lastName.toLowerCase()) {
             return 1;
         }
         return 0;
-      });
-      $('ul').remove();
-      $('section').append($ul);
-      for (var i =0; i <tab.length; i++){
+    });
+    $('ul').remove();
+    $('section').append($ul);
+    for (var i = 0; i < tab.length; i++) {
         $('ul').append($("<li>").text(tab[i].firstName + ' ' + tab[i].lastName));
 
-  }
+    }
 
 
 
@@ -27,16 +34,16 @@ function generateList(tab) {
 }
 
 function addPerson() {
-  var $input = $('#name');
-  var name = $input.val();
-  var $input = $('#lastname');
-  var lastname = $input.val();
-  var Person = {
-    firstName: name,
-    lastName: lastname
-  };
-  allPersons.push(Person);
-  generateList(allPersons);
-  localStorage.setItem("allPersons", JSON.stringify(allPersons));  
+    var $input = $('#name');
+    var name = $input.val();
+    var $input = $('#lastname');
+    var lastname = $input.val();
+    var Person = {
+        firstName: name,
+        lastName: lastname
+    };
+    allPersons.push(Person);
+    generateList(allPersons);
+    localStorage.setItem("allPersons", JSON.stringify(allPersons));
 }
 
